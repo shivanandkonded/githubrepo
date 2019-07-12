a26f5baf1cf25bd1b3ccdae65f169a83d3361575
shiva <shivanandkonded@gmail.com>
2019-07-12 10:10:34 +0530

    made changes on 12th july 2019

--- TestGitProject/src/com/gittest/HelloGitExample.java
+++ TestGitProject/src/com/gittest/HelloGitExample.java
@@ -2,6 +2,7 @@ package com.gittest;
 
 public class HelloGitExample {
 	public static void main(String[] args) {
+		System.out.println(" made changes for code");
 		System.out.println(" Hello This is Example for Eclipse to Github Repo");
 	}
 }
